413dee067699598def35702f94181d4f50acddca
Shaun Fowler <shaun.fowler@gmail.com>
2017-09-29 19:57:10 -0400

    setup seeding of mock data

---
+++ mock/import-mock-data-to-mongo.sh
@@ -0,0 +1,4 @@
+mongoimport --host sp_mongo:27017 --db sp --collection media      --type json --file /tmp/media.json      --jsonArray --drop
+mongoimport --host sp_mongo:27017 --db sp --collection updates    --type json --file /tmp/updates.json    --jsonArray --drop
+mongoimport --host sp_mongo:27017 --db sp --collection properties --type json --file /tmp/properties.json --jsonArray --drop
+mongoimport --host sp_mongo:27017 --db sp --collection locations  --type json --file /tmp/locations.json  --jsonArray --drop
