c6dcc7c0b23e1b87204c0dcd6f7195a1f61074b8
Shaun Fowler <shaunfowler@users.noreply.github.com>
2017-10-02 20:59:37 -0400

    add updates endpoint (#8)

---
+++ routes/updates.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var Guid = require("guid");
+var router = express.Router();
+var authMiddleware = require("../auth/middleware");
+var Model = require("../schema/update");
+
+const getAll = (req, res) => {
+    Model.find((error, properties) => {
+        res.json(properties);
+    });
+};
+
+const create = (req, res) => {
+    var update = new Model(req.body);
+    update._id = Guid.raw();
+    update.created = new Date().toString();
+    update.save(error => {
+        if (error) {
+            res.json({ error: error });
+            return;
+        }
+        res.json({ info: "created" });
+    });
+};
+
+const getById = (req, res) => {
+    Model.find({ _id: req.params.id }, (error, update) => {
+        if (error) {
+            res.json(error);
+            return;
+        }
+
+        if (update) {
+            res.json(update);
+        } else {
+            res.json({ info: "not found" });
+        }
+    });
+};
+
+const deleteById = (req, res) => {
+    Model.findOneAndRemove({ _id: req.params.id }, error => {
+        if (error) {
+            res.json({ error: error });
+            return;
+        }
+        res.json({ info: "removed" });
+    });
+};
+
+router.route("/").get(getAll);
+router.route("/").post(authMiddleware, create);
+router.route("/:id").get(getById);
+router.route("/:id").delete(authMiddleware, deleteById);
+
+module.exports = router;
