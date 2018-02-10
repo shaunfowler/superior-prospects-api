df75fb0a0278dbcaee1ef0f06f7f17ea394273b6
Shaun Fowler <shaun.fowler@qlik.com>
2018-02-10 13:02:55 -0500

    no auth mode

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,5 +1,7 @@
+const process = require("process");
+
 module.exports = (req, res, next) => {
-    if (req.isAuthenticated()) {
+    if (req.isAuthenticated() || process.env.BYPASS_AUTH === "true") {
         return next();
     }
     console.warn("401 -", req.method, req.originalUrl);
